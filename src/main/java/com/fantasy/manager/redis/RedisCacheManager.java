59873f8408af32bb0cb9c9b6fd851db2e1ebe223
hwangfantasy <fan.huang@ideacome.com>
2017-06-01 18:22:23 +0800

    shiro config

---
+++ src/main/java/com/fantasy/manager/redis/RedisCacheManager.java
@@ -0,0 +1,50 @@
+package com.fantasy.manager.redis;
+
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.io.Serializable;
+
+/**
+ * @作者 yunfeiyang
+ * @创建时间: 2017/6/1 <br/>
+ * @方法描述: RedisCacheManager. <br/>
+ */
+
+public class RedisCacheManager implements CacheManager, Serializable {
+
+    private transient static Logger log = LoggerFactory.getLogger(RedisCacheManager.class);
+
+    private transient RedisTemplate<Object, Object> redisTemplate;
+
+    public RedisCacheManager() {
+    }
+
+    public RedisCacheManager(RedisTemplate<Object, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public <K, V> Cache<K, V> getCache(String name) throws CacheException {
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("Cache name cannot be null or empty.");
+        }
+        log.debug("redis cache manager get cache name is :{}", name);
+        Cache cache = (Cache) redisTemplate.opsForValue().get(name);
+        if (cache == null) {
+            cache = new RedisCache<>(redisTemplate);
+            redisTemplate.opsForValue().set(RedisCache.shiro_cache_prefix + name, cache);
+        }
+        return cache;
+    }
+
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+}
+
