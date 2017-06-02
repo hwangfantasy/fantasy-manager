ebb0448f394eb17a757444b377fc3d76694bdce8
hwangfantasy <fan.huang@ideacome.com>
2017-06-02 14:31:36 +0800

    login

--- src/main/java/com/fantasy/manager/controller/LoginController.java
+++ src/main/java/com/fantasy/manager/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.fantasy.manager.controller;
 
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @作者 yunfeiyang
@@ -11,8 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController extends AbstractController {
 
-    @RequestMapping(value = {"/","/login"})
-    public String login(){
+    @RequestMapping(value = {"/login"})
+    public String showLoginForm(HttpServletRequest req, Model model) {
+        String exceptionClassName = (String) req.getAttribute("shiroLoginFailure");
+        String error = null;
+        if (UnknownAccountException.class.getName().equals(exceptionClassName)) {
+            error = "用户名/密码错误";
+        } else if (IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
+            error = "用户名/密码错误";
+        } else if (exceptionClassName != null) {
+            error = "其他错误：" + exceptionClassName;
+        }
+        model.addAttribute("error", error);
         return "login";
     }
 }
